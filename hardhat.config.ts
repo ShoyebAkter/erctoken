3ccc50d55f62c34f0d649ee7eb4b7928f9b6e4a1
Shoyeb180 <shoyebmohammad660@gmail.com>
2022-07-13 08:36:37 +0600

    first commit

---
+++ hardhat.config.ts
@@ -0,0 +1,11 @@
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+import "@nomiclabs/hardhat-ethers";
+
+require("@nomiclabs/hardhat-ethers");
+
+const config: HardhatUserConfig = {
+  solidity: "0.8.11",
+};
+
+export default config;
